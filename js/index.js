a6e6815464ead006ea7d827e52ec18da3f0fa099
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-28 12:51:42 -0400

    add gitCommits function

--- js/index.js
+++ js/index.js
@@ -2,16 +2,25 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
   let username = document.querySelector('#username').value
-  req.addEventListener('load', showRepositories)
+  req.addEventListener('load', displayRepositories)
   req.open('GET', `https://api.github.com/users/${username}/repos`)
   req.send();
 }
 
-function showRepositories() {
+function displayRepositories() {
   let repos = JSON.parse(this.responseText);
   let repoList = `
-    <ul>${repos.map(repo => '<li>' + repo.name + ':   ' + repo.html_url + '</li>')}
+    <ul>${repos.map(repo => '<li>' + repo.name + ' - ' + '<a href=' + repo.html_url + '>' + 'URL' + '</a>' + '- <a href="#" data-repo="' + repo.name + '" onclick="getCommits(this)">Get Commits</a>' + '</li>')}
     </ul>
   `
   document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  let repoName = el.dataset.repo;
+  let req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/' + ownerName + '/' + repoName + '/commits');
+  req.send();
+
 }
