24a66e5593d967dbbb754c180384777a8b42dd17
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-28 16:03:52 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -9,18 +9,43 @@ function getRepositories() {
 
 function displayRepositories() {
   let repos = JSON.parse(this.responseText);
+  //debugger
   let repoList = `
-    <ul>${repos.map(repo => '<li>' + repo.name + ' - ' + '<a href=' + repo.html_url + '>' + 'URL' + '</a>' + '- <a href="#" data-repo="' + repo.name + '" onclick="getCommits(this)">Get Commits</a>' + '</li>')}
+    <ul>${repos.map(repo => '<li>' + repo.name + ' - ' + '<a href=' + repo.html_url + '>' + 'URL' + '</a>' + '- <a href="#" data-repo1="' + repo.name + '" data-repo2="' + repo.owner.login + '" onclick="getCommits(this)">Get Commits</a>' + '</li>')}
     </ul>
   `
   document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getCommits(el) {
-  let repoName = el.dataset.repo;
+  // debugger
+  let repoName = el.dataset.repo1;
+  let repoOwnerLogin = el.dataset.repo2;
+
   let req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
-  req.open('GET', 'https://api.github.com/repos/' + ownerName + '/' + repoName + '/commits');
+  req.open('GET', 'https://api.github.com/repos/' + repoOwnerLogin + '/' + repoName + '/commits');
   req.send();
+}
 
+function displayCommits() {
+  let commits = JSON.parse(this.responseText);
+    // debugger
+  let commitsList = `<ul>
+    ${commits.map(
+      function(commit) {
+
+        // debugger
+        let li =
+        '<li>' +
+        // commit.author.login +
+        commit.commit.author.name +
+        commit.commit.message +
+        '</li>'
+
+        return li
+      }
+  )}
+  </ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
