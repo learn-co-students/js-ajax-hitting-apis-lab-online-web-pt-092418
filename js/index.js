eef02f4b3325fd2689701a37421a741b0916c618
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-28 11:22:01 -0400

    add getRepositories and showRepositories

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  let username = document.querySelector('#username').value
+  req.addEventListener('load', showRepositories)
+  req.open('GET', `https://api.github.com/users/${username}/repos`)
+  req.send();
+}
+
+function showRepositories() {
+  let repos = JSON.parse(this.responseText);
+  let repoList = `
+    <ul>${repos.map(repo => '<li>' + repo.name + ':   ' + repo.html_url + '</li>')}
+    </ul>
+  `
+  document.getElementById('repositories').innerHTML = repoList;
+}
